69ae01e4f1dbac4e93c12110792f5df4d6ad1733
Michael Dresser <michaelmdresser@gmail.com>
2020-02-24 22:11:15 -0700

    adding damage, etc - I see why people don't do only JS

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+var statusTable = document.getElementById("status-table")
+
+// https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+
+function sortByInitiative() {
+  // var newTable = document.createElement("table");
+  var table = statusTable
+
+  th = document.getElementById("initiative-header")
+
+  let elements = Array.from(table.querySelectorAll('tr:nth-child(n+2)'));
+  let th_index = Array.from(th.parentNode.children).indexOf(th);
+  console.log("th_index: ", th_index)
+  let sorted = elements.sort(comparer(th_index, false));
+  sorted.forEach(tr => table.appendChild(tr));
+
+  // document.getElementById("main-turn-order").appendChild(newTable);
+}
+
+
+
 function updateHealth(row) {
   console.log("updating row id: " + row.id)
   var maxHealth = row.getElementsByClassName("max-health")[0]
@@ -21,6 +48,19 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+function damagePlayer(e) {
+  // e.preventDefault()
+  console.log("in listener")
+  row = e.target.closest("tr")
+  damageTaken = row.getElementsByClassName("damage-taken")[0]
+  console.log("taken so far: ", damageTaken.innerText)
+  newDamage = e.currentTarget.parentElement.children[0]
+  damageTaken.innerText = parseInt(damageTaken.innerText) + parseInt(newDamage.value)
+  newDamage.value = ""
+
+  updateHealth(row)
+}
+
 function insertPlayerRow(table, name, initiativeModifier, maxHealth) {
   let rolledInitiative = rollInitiative(initiativeModifier)
   let newRow = table.insertRow(table.rows.length - 1)
@@ -31,17 +71,43 @@ function insertPlayerRow(table, name, initiativeModifier, maxHealth) {
   var cellInitiative = newRow.insertCell()
   var cellMaxHealth = newRow.insertCell()
   var cellDamageTaken = newRow.insertCell()
+  var cellDamage = newRow.insertCell()
   var cellCurrentHealth = newRow.insertCell()
   var cellStatusEffects = newRow.insertCell()
   
   cellName.innerText = name
   cellName.className = "name"
+
   cellInitiative.innerText = rolledInitiative
   cellInitiative.className = "initiative"
+
   cellMaxHealth.innerText = maxHealth
   cellMaxHealth.className = "max-health"
-  cellDamageTaken.innerText = 0
+
   cellDamageTaken.className = "damage-taken"
+  cellDamageTaken.innerText = 0
+  // damageTakenInput = document.createElement("input")
+  // damageTakenInput.type = "text"
+  // damageTakenInput.value = 0
+  // damageTakenInput.addEventListener("input", update)
+  // cellDamageTaken.appendChild(damageTakenInput)
+
+  cellDamage.className = "damage"
+  //damageForm = document.createElement("form")
+  damageInput = document.createElement("input")
+  damageButton = document.createElement("p")
+  //cellDamage.appendChild(damageForm)
+  //damageForm.appendChild(damageInput)
+  //damageForm.appendChild(damageButton)
+  cellDamage.appendChild(damageInput)
+  cellDamage.appendChild(damageButton)
+
+  //damageForm.className = "damage-form"
+  damageInput.type = "text"
+  // damageButton.type = "submit"
+  damageButton.innerText = "DAMAGE"
+  damageButton.onclick = damagePlayer
+
   cellCurrentHealth.className = "current-health"
   cellStatusEffects.className = "status-effects"
 }
@@ -64,6 +130,7 @@ form_new = document.getElementById("new-player-form")
 form_new.addEventListener("submit", function(event) {
   newPlayer()
   form_new.reset()
+  sortByInitiative()
   update()
   event.preventDefault()
 })
